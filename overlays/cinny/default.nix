d61f52631aefb9a00c2adc7e013047a35a5c448f
Philipp <philipp@boehm.sh>
2025-07-24 16:09:47 +0200

    update(cinny): bump cinny fork to newest version

--- overlays/cinny/default.nix
+++ overlays/cinny/default.nix
@@ -8,16 +8,16 @@ final: prev: {
   awesome-flake = (prev.awesome-flake or { }) // {
     cinny = prev.cinny-unwrapped.overrideAttrs (_old: rec {
       pname = "cinny-unwrapped";
-      version = "325144d8b2ca53c32fc6e1eace0603968a5ddc40";
+      version = "65475050d76d6e8da8c3402528215b1425e8ed4e";
 
       src = final.fetchFromGitHub {
         owner = "GigiaJ";
         repo = "cinny";
         rev = version;
-        hash = "sha256-822P12rzSLzje7KuBF2RB70SPdfCaHZaPV/1Nr4CCnY=";
+        hash = "sha256-kJZDc53mcJrGIw3Dl4ANq+1O5O2p0tcO2btQGNGRg4A=";
       };
 
-      npmDepsHash = "sha256-pP7JH/K9QSqyUVg0UFTDzZvRoL5CeP5pudv83eHVoTo=";
+      npmDepsHash = "sha256-GkD+CrblXBv7yPVrTBVIGkz7Wu5llWzlluNq7rmm3CE=";
       npmDeps = final.fetchNpmDeps {
         inherit src;
         name = "${pname}-${version}-npm-deps";
