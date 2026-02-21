2e03be5790838646a676c605a694926c452b3517
Philipp <philipp@boehm.sh>
2026-02-21 15:45:28 +0100

    feat(obs): add vkcapture

--- modules/home/apps/obs/default.nix
+++ modules/home/apps/obs/default.nix
@@ -16,12 +16,21 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      vulkan-tools
+    ];
+
+    home.sessionVariables = {
+      OBS_VKCAPTURE = "1";
+    };
+
     programs.obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
         obs-backgroundremoval
         obs-pipewire-audio-capture
+        obs-vkcapture
       ];
     };
   };
