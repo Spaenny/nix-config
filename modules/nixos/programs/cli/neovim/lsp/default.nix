9fdfe9199c4687889552f17181d9153e93c9d31d
Philipp <philipp@boehm.sh>
2026-08-07 17:09:42 +0200

    refactor: use nvf standard save formatting

--- modules/nixos/programs/cli/neovim/lsp/default.nix
+++ modules/nixos/programs/cli/neovim/lsp/default.nix
@@ -2,13 +2,6 @@
   programs.nvf.settings.vim = {
     formatter.conform-nvim = {
       enable = true;
-      setupOpts = {
-        default_format_opts.lsp_format = "fallback";
-        format_on_save = {
-          lsp_format = "fallback";
-          timeout_ms = 500;
-        };
-      };
     };
 
     lsp = {
